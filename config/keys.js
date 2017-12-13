a9a262437fadcdbd4d192137f9700335ce766d42
Juan Monsalve <jmonsalve@sproutloud.com>
2017-12-13 15:36:31 -0500

    oauth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// https://console.developers.google.com/apis
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
