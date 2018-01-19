b00ebb30e461cd6b457bc3d7d51b6b55ddb7c034
Juancho <juan.kmg92@gmail.com>
2018-01-18 21:15:42 -0500

    header and landing page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,14 +8,20 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
-  app.get('/logout', (req, res) => {
+  app.get('/auth/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
-  app.get('/api/user', (req, res) => {
-    res.send(req.user);
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user || null);
   });
 };
