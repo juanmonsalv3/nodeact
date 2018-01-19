c1c4b908d8fce8832f5a62b968c84e64d1c7e918
Juan Monsalve <jmonsalve@sproutloud.com>
2018-01-19 18:02:17 -0500

    first implementation of stripe

---
+++ client/src/components/Payments.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Nodeact-App"
+        description="Pay to Win :D"
+        amount={500}
+        token={token => this.props.handlePayment(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payments);
