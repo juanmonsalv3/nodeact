cdf227c1db86be2df3884889c31320135eedd69b
j.monsalve <j.monsalve@astoundcommerce.com>
2018-08-13 11:43:23 -0500

    organize files

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/User');
-require('./models/Survey');
+const model = require('./services/model')();
 require('./services/passport');
-
-mongoose.connect(keys.mongoURI);
 
 const app = express();
 
